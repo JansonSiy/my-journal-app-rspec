6fd8283e3b2f3b2a4e80f613eebd44b3e4a87bb6
Janson <jansonharold.siy@gmail.com>
2021-07-31 00:14:48 +0800

    added features/create_spec.rb

---
+++ spec/features/create_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper.rb'
+
+feature 'Creating Posts' do
+    scenario 'can create new posts' do
+        # visit root route
+        visit '/posts'
+
+        # click create post link
+        click_link 'Create Post'
+
+        # fill in the form with needed information
+        fill_in 'Title', with: 'title'
+        fill_in 'Category', with: 'category'
+        fill_in 'Body', with: 'body'
+
+        # click submit button
+        click_link 'Add Post'
+
+        # expect page to have the content we submitted
+        expect(page).to have_content('title')
+        expect(page).to have_content('category')
+        expect(page).to have_content('body')
+    end
+end
